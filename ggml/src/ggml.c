792dd1ff5ad2da7fbf8b59da68e2f02d47ff064e
Hermes <hermes@sixvpn.net>
2026-04-20 22:41:48 +0200

    fix: restore HISA assertions

--- ggml/src/ggml.c
+++ ggml/src/ggml.c
@@ -1075,7 +1075,7 @@ static const char * GGML_OP_NAME[GGML_OP_COUNT] = {
     "GLU",
 };
 
-// static_assert(GGML_OP_COUNT == 104, "GGML_OP_COUNT != 100");
+static_assert(GGML_OP_COUNT == 104, "GGML_OP_COUNT != 100");
 
 static const char * GGML_OP_SYMBOL[GGML_OP_COUNT] = {
     "none",
@@ -1185,7 +1185,7 @@ static const char * GGML_OP_SYMBOL[GGML_OP_COUNT] = {
     "glu(x)",
 };
 
-// static_assert(GGML_OP_COUNT == 104, "GGML_OP_COUNT != 100");
+static_assert(GGML_OP_COUNT == 104, "GGML_OP_COUNT != 100");
 
 static_assert(GGML_OP_POOL_COUNT == 2, "GGML_OP_POOL_COUNT != 2");
 
